353caf97c7857acf8ed27abcf431fc0dcb3a8fdd
Phạm Lê Minh Phú <34944968+phamleminhphu@users.noreply.github.com>
2019-05-06 08:43:34 +0700

    Submit

---
+++ Project/Nhom1AppMusic/app/src/main/java/com/naman14/timber/activities/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.naman14.timber.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.naman14.timber.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },SPLASH_TIME);
+    }
+}
